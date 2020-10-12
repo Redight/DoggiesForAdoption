03f956c526d013f38554caee4c7394500e405176
Sofiane Debbagh <red@pop-os.localdomain>
2020-10-12 21:19:28 +0100

    the first get request

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ server.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-server.listen(port, ()=> console.log('\n on port ${port} \n'))
+server.listen(port, ()=> console.log(`\n Running on port ${5000}\n`))
+
+server.get('/', (req, res) =>{
+    res.send("woof woof ! We out the Pound!")
+});
+
+server.get('/yes', (req, res) =>{
+    res.send("Starting to get the hang of it!")
+});
